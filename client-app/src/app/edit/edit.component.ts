8b8e985b7b13605fff9132af9887969911bcd9e2
Samandar <uchqunovsamandar@mail.ru>
2022-08-01 09:36:58 -0700

    basic ui

--- client-app/src/app/edit/edit.component.ts
+++ client-app/src/app/edit/edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { pipe } from 'rxjs';
+import { UserVM } from '../models/model';
 
 @Component({
   selector: 'app-edit',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private router:Router) { }
+  me = new UserVM('1', 'https://findicons.com/files/icons/2526/bloggers/256/admin.png', 'samandar@mail.ru', '', 'samandar', 'Im full stack dev', '974908957')
   ngOnInit(): void {
+  }
+
+  OnSubmit(){
+    
+    this.router.navigate(['me'])
   }
 
 }
--- client-app/src/app/me/me.component.ts
+++ client-app/src/app/me/me.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserVM } from '../models/model';
 
 @Component({
   selector: 'app-me',
@@ -8,8 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class MeComponent implements OnInit {
 
   constructor() { }
-
+  me = new UserVM('1', 'https://findicons.com/files/icons/2526/bloggers/256/admin.png', 'samandar@mail.ru', '', 'samandar', 'Im full stack dev', '974908957')
   ngOnInit(): void {
+
   }
 
 }
